2641ae86f810e7e5f34a584bd3548b0400c141ca
hikerpig <hikerpigwinnie@gmail.com>
2022-05-18 22:58:50 +0800

    chore(demo): text area web component

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -5,9 +5,15 @@ import './style.css'
 
 const pintoraContainer = document.querySelector('.pintora') as HTMLDivElement
 const editor = document.getElementById('editor') as HTMLTextAreaElement
+const updateBtn = document.getElementById('update-btn')
 
 const form = document.getElementById('form')! as HTMLFormElement
 const config: Partial<PintoraConfig['pie']> = {}
+const INIT_SOURCE = `pie 
+  title Bag of Fruits
+  "apple" 5
+  "peach" 6 
+  "banana" 20`
 
 function updatePreview() {
   const resultContainer = pintoraContainer.parentElement?.querySelector(
@@ -25,6 +31,7 @@ function updatePreview() {
 
 form.addEventListener('change', (e: any) => {
   const { target } = e
+  console.log('form change', target)
   let changed = false
   let v
   if (target.type === 'number') {
@@ -44,10 +51,27 @@ form.addEventListener('change', (e: any) => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
+  e.stopPropagation()
+  console.log('submit')
 })
 
 editor.addEventListener('input', (e) => {
   updatePreview()
 })
 
+updateBtn!.addEventListener('click', () => {
+  const formData = new FormData(form)
+  const data: any = {}
+  formData.forEach((v, k) => {
+    data[k] = v
+  })
+  data.circleRadius = parseFloat(data.circleRadius)
+  pintora.setConfig({
+    pie: data,
+  })
+
+  updatePreview()
+})
+
+editor.value = INIT_SOURCE
 updatePreview()
